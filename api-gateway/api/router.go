dddc74cd1183773e231c1a9cd0ddad448f4c3feb
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2024-08-12 12:54:21 +0500

    cors fixed

--- api-gateway/api/router.go
+++ api-gateway/api/router.go
@@ -69,18 +69,15 @@ func NewRouter(PollConn *grpc.ClientConn) *gin.Engine {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Content-Type", "application/json")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusInsufficientStorage)
+			c.AbortWithStatus(http.StatusOK)
 			return
-		} else {
-			c.Next()
 		}
+
+		c.Next()
 	}
 }
