7109b1be98f3f3a3021bdcd60238cc66a414a819
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2024-08-12 17:11:00 +0500

    done

--- api-gateway/api/router.go
+++ api-gateway/api/router.go
@@ -25,9 +25,10 @@ func NewRouter(PollConn *grpc.ClientConn) *gin.Engine {
 	router := gin.Default()
 	h := handlers.NewHandler(PollConn)
 	wd, _ := os.Getwd()
-	filesDir := filepath.Join(wd, "files")
-	router.Static("/files", filesDir)
+	filesDir := filepath.Join(wd, "files/")
+	router.Static("/files/", filesDir)
 	fmt.Println(wd)
+	fmt.Println(filesDir)
 
 	router.Use(CORSMiddleware())
 
