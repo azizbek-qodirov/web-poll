636854ef7cccb9b34e42e24c71b2238150a705e8
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2024-08-30 16:18:34 +0500

    done

--- api-gateway/config/helperFuncs.go
+++ api-gateway/config/helperFuncs.go
@@ -14,17 +14,8 @@ func IsValidEmail(email string) bool {
 }
 
 func IsValidPassword(password string) error {
-	if len(password) < 8 {
-		return errors.New("password must be at least 8 characters long")
-	}
-	if !regexp.MustCompile(`[0-9]`).MatchString(password) {
-		return errors.New("password must contain at least one number")
-	}
-	if !regexp.MustCompile(`[A-Z]`).MatchString(password) {
-		return errors.New("password must contain at least one uppercase letter")
-	}
-	if !regexp.MustCompile(`[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]`).MatchString(password) {
-		return errors.New("password must contain at least one special character")
+	if len(password) <= 4 {
+		return errors.New("password must be at least 4 characters long")
 	}
 	return nil
 }
--- polling/service/result.go
+++ polling/service/result.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	pb "poll-service/genprotos"
 
 	st "poll-service/storage"
@@ -41,34 +42,33 @@ func (s *ResultService) GetPollResults(ctx context.Context, req *pb.ByIDs) (*pb.
 	}
 	if *poll.PollNum == 1 {
 		for _, v := range resAnswer.Answers {
-			switch *v.Num {
-			case 1, 3, 8, 10, 13, 17, 22, 25, 27, 39, 44, 46, 49, 53, 56:
+			switch {
+			case isIn(*v.Num, []int32{1, 3, 8, 10, 13, 17, 22, 25, 27, 39, 44, 46, 49, 53, 56}) && *v.AnswerPoint == int32(1):
 				extrovert += 1
-			case 2, 4, 7, 9, 11, 14, 16, 19, 21, 23, 26, 28, 31, 33, 35, 38, 40, 43, 45, 47, 50, 52, 55, 57:
+			case isIn(*v.Num, []int32{5, 15, 29, 32, 34, 37, 41, 51}) && *v.AnswerPoint == int32(0):
+				extrovert += 1
+			case isIn(*v.Num, []int32{2, 4, 7, 9, 11, 14, 16, 19, 21, 23, 26, 28, 31, 33, 35, 38, 40, 43, 45, 47, 50, 52, 55, 57}) && *v.AnswerPoint == int32(1):
 				nevrotizm += 1
 			default:
 				continue
 			}
 		}
+		feed += "Siz "
 
-		if extrovert >= 12 {
-			feed = "Siz ekstravert"
+		if extrovert > 12 && extrovert > nevrotizm {
+			feed = "Extrovert"
+		} else if extrovert < 12 && nevrotizm < 12 {
+			feed = "Introvert"
+		} else if extrovert == nevrotizm && extrovert > 12 && nevrotizm < 12 {
+			feed = "Extrovert va Nevrotizm"
 		} else {
-			feed = "Siz introvert"
+			feed = "Nevrotizm"
 		}
-
-		if nevrotizm > 12 && extrovert < 12 {
-			feed += " va nevrotizm"
-		} else if nevrotizm > 12 {
-			feed += " va nevrotizm"
-		}
-		feed += " turiga mansubsiz"
 
 	} else {
 		for _, v := range resAnswer.Answers {
 			total += *v.AnswerPoint
 		}
-
 		for _, v := range poll.Feedback {
 			if total >= *v.From && total < *v.To {
 				feed = *v.Text
@@ -77,6 +77,16 @@ func (s *ResultService) GetPollResults(ctx context.Context, req *pb.ByIDs) (*pb.
 		}
 	}
 
+	fmt.Println(resAnswer.Feed)
 	resAnswer.Feed = &feed
 	return resAnswer, nil
+}
+
+func isIn(num int32, ls []int32) bool {
+	for _, v := range ls {
+		if v == num {
+			return true
+		}
+	}
+	return false
 }
